c62017f6e5cc8d20d17369f565f60b3c971b66f3
958691165 <958691165@qq.com>
2018-05-27 23:42:40 +0800

    init

---
+++ index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+var rf=require("fs");
+
+app.get('/webhook', function (req, res) {
+
+    var data=rf.readFileSync("config.json","utf-8");
+    data=JSON.parse(data);
+    var pro=req.query.pro;
+    if (data[pro]){
+
+        var commands = [
+            'cd ' + data[pro],
+            'D:',
+            'git pull'
+        ].join(' && ');
+
+        console.log(commands);
+        require('child_process').exec(commands, function(err, out, code) {
+            if (err instanceof Error) {
+                res.status(500);
+                res.send(code);
+            }else {
+                res.send('ok');
+            }
+
+        });
+    } else{
+        res.send('pro not find');
+    }
+});
+
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+});
