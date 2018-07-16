91a86cf1ec8ca5db1ccef95869b6445bcc370917
Jack <958691165@qq.com>
2018-07-17 00:12:59 +0800

    all complete

--- README.md
+++ README.md
@@ -2,8 +2,15 @@
 用于git的webhook服务,用法：<br>
   ````
   1.[c]npm install
-  2.node app.js
+  2.node index.js 或 npm run start
   3.配置config.json
-  4.地址:
-    [ip]:[port]/webhook/[项目名]
+  4.webhook地址:
+    http://[ip]:[port]/webhook/[项目名]
   ````
+  PS:<br>
+  <ul>
+    <li>本程序配有后台设置页面如有需要请到<a href="#">http://[您的ip]:[设置的端口]</a></li>
+    <li>后台默认密码为123456，部署后请务必修改</li>
+    <li>强烈建议不要用根用户运行此程序，避免出现不必要的损失</li>
+    <li>若有疑问请发邮件至958691165@qq.com</li>
+  </ul>
--- src/webhook.js
+++ src/webhook.js
@@ -27,13 +27,22 @@ router.get('/*',function(req,res){
                 break;
         }
         var extra_commands=project.commands.split("\n");
-        commands=commands.concat(extra_commands);
+        if (extra_commands){
+            for (i in extra_commands){
+                extra_commands[i]=extra_commands[i].trim();
+                if (extra_commands[i]){
+                    commands.push(extra_commands[i]);
+                }
+            }
+        }
 
         commands=commands.join(' && ');
+        units.log(commands);
         require('child_process').exec(commands, function(err, out, code) {
             if (err instanceof Error) {
                 result.code=500;
                 result.msg=""+code;
+                units.log(code+"");
             }else {
                 result.code=200;
                 result.msg="command run ok!";
